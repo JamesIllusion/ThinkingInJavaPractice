574e4d7b2bfaf817848246c7547d0bf667d16a9c
autotest <autotest@SpidermanBak1.(none)>
2014-12-04 16:42:09 -0600

    20141204 modify 'Frog' from SpiderBak_Ubuntu

--- src/Frog.java
+++ src/Frog.java
@@ -56,8 +56,34 @@ class Amphibian extends Animal{
 	Amphibian(){
 		System.out.println("Amphibian.constructor");
 	}
+
+	protected void finalize(){
+		System.out.println("Amphibian.finalize");
+
+		if(DoBaseFinalization.flag)
+			try{
+				super.finalize();
+			} catch(Throwable t){}
+	}
 }
 
-public class Frog {
+public class Frog extends Amphibian{
+	Frog(){
+		System.out.println("Frog.constructor");
+	}
 
+	protected void finalize(){
+		System.out.println("Frog.finalize");
+
+		if(DoBaseFinalization.flag)
+			try{
+				super.finalize();
+			} catch(Throwable t){}
+	}
+
+	public static void main(String[] args){
+		if(args.length !=0 && args[0].equals("finalize"))
+			DoBaseFinalization.flag = true;
+		else
+			System.out.println("not finalizing bases");
 }
