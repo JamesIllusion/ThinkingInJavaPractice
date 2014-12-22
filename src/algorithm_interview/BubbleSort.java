6920cf76da2c7f5e99e57888160abd8ce46f728b
JamesIllusion <james.lt.xu@gmail.com>
2014-12-22 16:07:19 -0600

    20141222 submit 'bubbleSort' 'insertionsort' from Spiderbak_Ubuntu

---
+++ src/algorithm_interview/BubbleSort.java
@@ -0,0 +1,23 @@
+package algorithm_interview;
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int array[] = {3,5,8,4,6,1,2,7,9,0};
+		int temp = 0;
+		
+		for (int i=0; i< array.length-1; i++)
+			for (int j=0; j<array.length-i-1; j++)
+				if(array[j] > array[j+1]){
+					temp = array[j];
+					array[j] = array[j+1];
+					array[j+1] = temp;
+				}
+		
+		for (int i=0; i<array.length; i++)
+			System.out.print(array[i] + ", ");
+	}
+
+}
---
+++ src/algorithm_interview/InsertionSort.java
@@ -0,0 +1,11 @@
+package algorithm_interview;
+
+public class InsertionSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int array[] = {3,5,8,4,6,1,2,7,9,0};
+	}
+
+}
