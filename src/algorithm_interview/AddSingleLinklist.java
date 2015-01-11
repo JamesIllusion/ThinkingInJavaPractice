9161f6bc4dda89cb0168df3366b05857acff87a6
JamesIllusion <james.lt.xu@gmail.com>
2015-01-10 22:49:48 -0600

    20150110 Change 'AddSingleLinklist'

--- src/algorithm_interview/AddSingleLinklist.java
+++ src/algorithm_interview/AddSingleLinklist.java
@@ -23,10 +23,10 @@ public class AddSingleLinklist {
 			answer.data = 1;
 			mark = answer;
 			answer = answer.next;
-			answer.data = (a.data+b.data)%10;
+			answer.data = (a.data+b.data)-10;
 		}
 		else{
-			answer.data = (a.data+b.data)%10;
+			answer.data = a.data+b.data;
 			mark = answer;
 			answer = answer.next;
 		}
@@ -36,7 +36,7 @@ public class AddSingleLinklist {
 		// calculation loop
 		while(a!=null && b!=null){
 			if((a.data+b.data)>9){
-				answer.data = (a.data+b.data)%10;
+				answer.data = (a.data+b.data)-10;
 				answer.next = answer;
 				
 				while(mark!=answer){
@@ -69,6 +69,30 @@ public class AddSingleLinklist {
 			System.out.print(node.data + " -> ");
 			node = node.next;
 		}System.out.println(node.data);
+	}
+	
+	public static void main(String[] args){
+		Node a_1 = new Node(2);
+		Node a_2 = new Node(5);
+		Node a_3 = new Node(7);
+		Node a_4 = new Node(9);
+		Node a_5 = new Node(0);
+		a_1.next = a_2;
+		a_2.next = a_3;
+		a_3.next = a_4;
+		a_4.next = a_5;
+		
+		Node b_1 = new Node(4);
+		Node b_2 = new Node(8);
+		Node b_3 = new Node(6);
+		Node b_4 = new Node(3);
+		Node b_5 = new Node(1);
+		b_1.next = b_2;
+		b_2.next = b_3;
+		b_3.next = b_4;
+		b_4.next = b_5;
+		
+		printList(addList(a_1, b_1));
 	}
 
 }
